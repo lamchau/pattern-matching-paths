c86beb71f7cdabf6befb637d1215cfddea8eaa79
Lam Chau <lamchau@gmail.com>
2015-09-22 23:16:25 -0400

    add missing test

--- test/com/warbyparker/WorkerTest.java
+++ test/com/warbyparker/WorkerTest.java
@@ -1,10 +1,11 @@
 package com.warbyparker;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class WorkerTest {
   private static final List<SimplePattern> PATTERNS = Arrays.asList(new SimplePattern("a"));
@@ -19,5 +20,40 @@ public class WorkerTest {
   public void testCallNoMatch() throws Exception {
     Worker worker = new Worker(PATTERNS, "b");
     Assert.assertEquals(worker.call(), "NO MATCH");
+  }
+
+  @Test
+  public void test() throws Exception {
+    List<SimplePattern> patterns = Arrays.asList(
+        new SimplePattern("a,b"),
+        new SimplePattern("a,b,c"),
+        new SimplePattern("foo,bar"),
+        new SimplePattern("*,*"),
+        new SimplePattern("*,bar")
+    );
+    Collections.sort(patterns);
+
+    List<String> paths = Arrays.asList(
+        "a/bar",
+        "hello/world",
+        "q",
+        "a/b",
+        "foo"
+    );
+
+    List<String> results = new ArrayList<String>();
+
+    for (String s : paths) {
+      Worker worker = new Worker(patterns, s);
+      results.add(worker.call());
+    }
+    Assert.assertArrayEquals(new String[] {
+        "*,bar",
+        "*,*",
+        "NO MATCH",
+        "a,b",
+        "NO MATCH"
+    }, results.toArray());
+
   }
 }
